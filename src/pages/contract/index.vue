b138576967007f55e2cccfaa3053fac7c8273acb
chrischen0405 <chrischen0405@gmail.com>
2022-05-09 18:21:23 +0800

    feat: 内容更新

--- src/pages/contract/index.vue
+++ src/pages/contract/index.vue
@@ -2,13 +2,21 @@
     <div class="container">
         <div class="main">
             <p class="tip">点击文本可复制</p>
-            <div class="box" style="margin-top: 40rpx" @click="copy('http://chrischen0405.github.io/')">
+            <div class="box" @click="copy('http://chrischen0405.github.io/')">
                 <img src="../../../static/images/home.png"/>
                 <p>http://chrischen0405.github.io/</p>
             </div>
             <div class="box" @click="copy('chrischen0405@qq.com')">
                 <img src="../../../static/images/email.png"/>
                 <p>chrischen0405@qq.com</p>
+            </div>
+            <div class="box" @click="copy('https://www.jianshu.com/u/c6a5a7f59d5f')">
+                <img class="blog-img" src="../../../static/images/blog.png"/>
+                <p>博客地址</p>
+            </div>
+            <div class="box" @click="copy('https://github.com/chrischen0405')">
+                <img src="../../../static/images/github.png"/>
+                <p>chrischen0405</p>
             </div>
             <div class="box">
                 <button open-type="contact">在线联系</button>
@@ -29,7 +37,7 @@ export default {
             mpvue.setClipboardData({
                 data: data,
                 success () {
-                    util.showToast('复制成功')
+                    util.showToast(`${data} 复制成功`)
                 }
             })
         }
@@ -41,37 +49,42 @@ export default {
 
 <style lang="scss" scoped>
 @import "../../styles/comm";
+
 .container {
-  padding: 40rpx 0;
-  width: 750rpx;
-  @include flex(column, flex-start, center);
-  .main {
-    width: 670rpx;
-    border-radius: 8rpx;
-    box-shadow: 0 10rpx 20rpx #1296db;
-    .tip {
-      margin: 40rpx 40rpx 0;
-      text-align: left;
-      @include font(24, #666, 40, 400);
+    padding: 40rpx 0;
+    @include w_h(750);
+    @include flex(column, flex-start, center);
+    .main {
+        @include w_h(670);
+        border-radius: 8rpx;
+        box-shadow: 0 10rpx 20rpx #1296db;
+        .tip {
+            margin: 40rpx 40rpx 40rpx;
+            text-align: left;
+            @include font(24, #666, 40, 400);
+        }
+        .box {
+            @include w_h(590, 80);
+            margin: 80rpx auto;
+            @include flex(row, flex-start, center);
+            border-radius: 8rpx;
+            box-shadow: 0 10rpx 20rpx #1296db;
+            img {
+                margin-left: 20rpx;
+                @include w_h(60, 60);
+            }
+            p {
+                margin-left: 20rpx;
+                @include font(32, #000, 40, 400);
+            }
+            button {
+                @include font(32, #000, 40, 400);
+            }
+            .blog-img {
+                @include w_h(45, 45);
+                margin-left: 30rpx;
+            }
+        }
     }
-    .box {
-      @include w_h(590, 80);
-      margin: 80rpx auto;
-      @include flex(row, flex-start, center);
-      border-radius: 8rpx;
-      box-shadow: 0 10rpx 20rpx #1296db;
-      img {
-        margin-left: 20rpx;
-        @include w_h(60, 60);
-      }
-      p {
-        margin-left: 20rpx;
-        @include font(32, #000, 40, 400);
-      }
-      button {
-        @include font(32, #000, 40, 400);
-      }
-    }
-  }
 }
 </style>
--- src/pages/skill/index.vue
+++ src/pages/skill/index.vue
@@ -2,24 +2,22 @@
     <div class="container">
         <div class="main">
             <ul>
-                <li>熟悉HTML5和CSS3，了解W3C标准和web语义化，会用ps切图，熟悉原生javascript或typescript开发，了解其基本语言特性，熟悉ES6新增特性</li>
-                <li>熟练使用jQuery，vue框架，使用过vue-cli，vuex，vue-route进行开发</li>
-                <li>熟悉微信小程序开发，使用过原生小程序以及mpvue，wepy等框架，进行过第三方小程序开发</li>
-                <li>了解响应式页面，会写css媒体查询，使用过bootstrap，使用过elementUI，ant design，weui，Materialize等UI库</li>
-                <li>了解CSS3动画，也能使用canvas作动画，使用过phaser、lufylegend等h5游戏引擎</li>
-                <li>明白get/post/put区别，了解http协议及浏览器原理，掌握前端问题的定位方法，了解基本的前端体验提升及性能优化</li>
-                <li>熟练掌握git进行协同开发</li>
-                <li>拥有flutter跨平台开发的实战经验，使用过uni-app进行APP开发并使用云打包，了解dart语法以及安卓ios部分差异</li>
-                <li>使用过服务端开发语言java，使用过springboot，jpa，了解mysql数据库，能写基本的sql语句</li>
+                <li v-for="(item, index) in list" :key="index">
+                    {{item}}
+                </li>
             </ul>
         </div>
     </div>
 </template>
 
 <script>
+import { skillList } from '../../utils/data'
+
 export default {
     data () {
-        return {}
+        return {
+            list: skillList
+        }
     },
     onShareAppMessage () {
     }
@@ -28,22 +26,23 @@ export default {
 
 <style scoped lang="scss">
 @import "../../styles/comm";
+
 .container {
-  padding: 40rpx 0;
-  width: 750rpx;
-  @include flex(column, flex-start, center);
-  .main {
-    width: 670rpx;
-    border-radius: 8rpx;
-    box-shadow: 0 10rpx 20rpx #1296db;
-    ul {
-      margin: 20rpx;
-      li {
-        @include font(28, #444, 40, 400);
-        text-align: left;
-        margin: 50rpx 20rpx;
-      }
+    padding: 40rpx 0;
+    @include w_h(750);
+    @include flex(column, flex-start, center);
+    .main {
+        @include w_h(670);
+        border-radius: 8rpx;
+        box-shadow: 0 10rpx 20rpx #1296db;
+        ul {
+            margin: 20rpx;
+            li {
+                @include font(28, #444, 40, 400);
+                text-align: left;
+                margin: 50rpx 20rpx;
+            }
+        }
     }
-  }
 }
 </style>
